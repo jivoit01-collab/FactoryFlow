46ed2f6bb2a4f67be2bee00175a019fa22bb1d9e
gurparvez <gurparvez04@gmail.com>
2026-03-21 13:41:05 +0530

    Fix folder casing to rawMaterialPages

--- src/modules/gate/pages/rawmaterialpages/AttachmentsPage.tsx
+++
@@ -1,6 +0,0 @@
-import { RAW_MATERIAL_FLOW } from '../../constants/entryFlowConfig';
-import SharedAttachmentsPage from '../shared/SharedAttachmentsPage';
-
-export default function AttachmentsPage() {
-  return <SharedAttachmentsPage config={RAW_MATERIAL_FLOW} />;
-}
--- src/modules/gate/pages/rawmaterialpages/RawMaterialsDashboard.tsx
+++
@@ -1,6 +0,0 @@
-import { RAW_MATERIAL_FLOW } from '../../constants/entryFlowConfig';
-import SharedDashboard, { RAW_MATERIAL_STATUS_CONFIG } from '../shared/SharedDashboard';
-
-export default function RawMaterialsDashboard() {
-  return <SharedDashboard config={RAW_MATERIAL_FLOW} statusConfig={RAW_MATERIAL_STATUS_CONFIG} />;
-}
